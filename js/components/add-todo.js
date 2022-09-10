cdfdaf0be0db47f77e7082a7a6aec4a6c25825d2
Andrés Ríos <al.andres.rios.lima@iesportada.org>
2022-09-10 13:36:36 +0200

    implement alert component

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,18 +1,18 @@
+import Alert from "./alert.js";
 export default class AddTodo{
     constructor() {
         this.title = document.getElementById('title');
         this.description = document.getElementById('description');
         this.btn = document.getElementById('add');
+        this.alert = new Alert('alert');
     }
 
     onClick(callback){
         this.btn.onclick = () =>{
             if (title.value === '' && description.value === '') {
-               /* alert.classList.remove('d-none');
-                alert.innerText = "The title and description are requered";
-                return;*/
-                console.error("Campos vacios");
+               this.alert.show('Titulo y descripción son obligatorios');
             } else {
+                this.alert.hide();
                 callback(this.title.value,this.description.value);
             }
         }
